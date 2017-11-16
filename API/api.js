2989b5ec4959f17cda5d7e1c25af9aeed563e891
Jeff Dederick <Xpressivmotion@gmail.com>
2017-11-16 17:34:33 -0500

    timer change

--- API/api.js
+++ API/api.js
@@ -7,28 +7,26 @@ class Gitwrap {
     git.authenticate({
       type: 'token',
       token: '6c5e98cc360a8d6b5646d94f5ad9f06f121ceaf4'
-    })
+    });
   }
 
-    getCommits(owner, repo) {
-        return new Promise((resolve, reject)=>{
-                git.repos.getCommits(
-                  {
-                      owner: `${owner}`,
-                      repo: `${repo}`
-                  },
-                  function (err, data) {
-                      if (err) {
-                          return reject(err);
-                      }
-                      return resolve(data);
-                  }
-                  );
-        })
-    }
-    //work with promises
-
+  getCommits(repo, owner) {
+    return new Promise((resolve, reject) => {
+      git.repos.getCommits(
+        {
+          owner: `${owner}`,
+          repo: `${repo}`
+        },
+        function(err, data) {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(data);
+        }
+      );
+    });
+  }
+  //work with promises
 }
 
-module.exports = Gitwrap
-
+module.exports = Gitwrap;
--- router.js
+++ router.js
@@ -9,74 +9,73 @@ const querystring = require('querystring');
 const Router = {};
 
 Router.handle = (req, res) => {
-    let method = req.method.toLowerCase();
-    let path = url.parse(req.url).pathname;
-    
-    if (path != "/favicon.ico") {
-        console.log(method, path);
-        Router.routes[method][path](req, res);   
-    }
+  let method = req.method.toLowerCase();
+  let path = url.parse(req.url).pathname;
+
+  if (path != '/favicon.ico') {
+    console.log(method, path);
+    Router.routes[method][path](req, res);
+  }
 };
 
 Router.routes = {
-    get: {
-        '/': (req, res) => {
-            fs.readFile('./views/index.html', function (err, html) {
-                if (err) {
-                    throw err;
-                }
-                res.writeHead(200, {
-                    'Content-Type': 'text/html'
-                });
-                
-                res.write(html.toString());
-                
-                res.end();
-            });
-        },
-        '/commits': (req, res) => {
-             fs.readFile('./views/index.html', function (err, html) {
-                if (err) {
-                    throw err;
-                }
-                res.writeHead(200, {
-                    'Content-Type': 'text/html'
-                });
-
-                let formData = querystring.parse(req.url);
-                console.log(formData);
-                
-                const gh = new gitApi(); 
-                
-                gh.getCommits(formData.repo, formData.owner)
-                 .then((data) => {
-                    res.write(html.toString().replace(/{{commitfeed}}/, data));
-
-                    res.end();
-                })
-                 .catch(
-                    console.log("placeholder");
-                )
-            })
+  get: {
+    '/': (req, res) => {
+      fs.readFile('./views/index.html', function(err, html) {
+        if (err) {
+          throw err;
         }
+        res.writeHead(200, {
+          'Content-Type': 'text/html'
+        });
+
+        res.write(html.toString());
+
+        res.end();
+      });
     },
-    post: {
-        '/': (req, res) => {
-            fs.readFile('./views/index.html', function (err, html) {
-                if (err) {
-                    throw err;
-                }
-                res.writeHead(200, {
-                    'Content-Type': 'text/html'
-                });
-
-                let data = JSON.stringify(data1, null, 2);
-
-                res.write(html.toString().replace(/{{commitfeed}}/, data));
-                res.end();
-            });
+    '/commits': (req, res) => {
+      fs.readFile('./views/index.html', function(err, html) {
+        if (err) {
+          throw err;
         }
+        res.writeHead(200, {
+          'Content-Type': 'text/html'
+        });
+
+        let formData = querystring.parse(url.parse(req.url).query);
+
+        const gh = new gitApi();
+        console.log(formData.repo, formData.owner);
+
+        gh
+          .getCommits(formData.repo, formData.owner)
+          .then(data => {
+            res.write(html.toString().replace(/{{commitfeed}}/, data));
+
+            res.end();
+          })
+          .catch(error => console.error(error));
+      });
     }
+  },
+  post: {
+    '/': (req, res) => {
+      fs.readFile('./views/index.html', function(err, html) {
+        if (err) {
+          throw err;
+        }
+        res.writeHead(200, {
+          'Content-Type': 'text/html'
+        });
+
+        let data = JSON.stringify(data1, null, 2);
+
+        res.write(html.toString().replace(/{{commitfeed}}/, data));
+        res.end();
+      });
+    }
+  }
 };
 
 module.exports = Router;
